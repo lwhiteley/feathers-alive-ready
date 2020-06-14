a4ea0b54f84dbc6a4c6632d98142afdd13b7d955
Layton Whiteley <layton.whiteley@gmail.com>
2020-06-14 23:55:49 +0200

    feat: add initial code

---
+++ src/index.ts
@@ -0,0 +1,59 @@
+import { BadRequest } from '@feathersjs/errors';
+import { Application } from '@feathersjs/express';
+
+interface HealthOptions {
+  configKey?: string;
+  returnData?: boolean;
+  aliveUrl?: string;
+  readyUrl?: string;
+}
+
+export default (opts?: HealthOptions) => {
+  const { configKey: ns, returnData, aliveUrl, readyUrl } = {
+    configKey: 'readiness',
+    returnData: false,
+    aliveUrl: '/health/alive',
+    readyUrl: '/health/ready',
+    ...(opts || {}),
+  };
+
+  return function (app: Application) {
+    app.set('setReady', (key: string) => {
+      const readiness = app.get(ns) || {};
+
+      if (Object.keys(readiness).includes(key)) {
+        app.set(ns, {
+          ...readiness,
+          [key]: true,
+        });
+      }
+    });
+
+    app.get(aliveUrl, (req, res) => {
+      res.status(204).send();
+    });
+
+    app.get(readyUrl, (req, res) => {
+      const readiness = req.app.get(ns) || {};
+      const values = Object.values(readiness);
+      const isReady = values.every((value) => value);
+      const details = returnData ? readiness : {};
+
+      if (!Object.keys(readiness).length) {
+        return res
+          .status(400)
+          .send(new BadRequest(`config.${ns} not configured`, { ...details }));
+      }
+
+      if (!isReady) {
+        return res
+          .status(400)
+          .send(new BadRequest('Application is not ready', { ...details }));
+      }
+
+      return res
+        .status(returnData ? 200 : 204)
+        .send(returnData ? readiness : undefined);
+    });
+  };
+};
